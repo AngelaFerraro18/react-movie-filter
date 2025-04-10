639d646383714a39a1431c89e00acec79c163dce
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-10 14:48:12 +0200

    feat: create filter to select a movie by genre, using a select tag html and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+//importo useEffect
+import { useEffect } from "react";
+//importo useState
+import { useState } from "react";
 
-const movieList = [
+const movies = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
@@ -11,11 +15,42 @@ const movieList = [
 
 function App() {
 
+  //salvo in una variabile il valore di partenza della lista dei film, che sarà una stringa vuota
+  const [moviesList, setMoviesList] = useState('');
+
+  //salvo in una variabile il valore di partenza del filtro dei film, che sarà il mio array di partenza
+  const [filteredMovie, setFilteredMovie] = useState(movies);
+
+  //vado ad usare useEffect per filtrare la mia lista
+  useEffect(() => {
+
+    //se la mia lista sarà vuota, allora mostrerò tutta la lista completa
+    if (moviesList === '') {
+      setFilteredMovie(movies);
+    } else {
+      //altrimenti vado a filtrare con filter e salvo il risultato della ricerca in una variabile: resultMovie
+      const resultMovie = movies.filter(movie => movie.genre === moviesList);
+
+      //vado ad usare la funzione per vedere in tempo reale la modifica all'UI il risultato della ricerca
+      setFilteredMovie(resultMovie);
+      console.log(resultMovie);
+    }
+  }, [moviesList]) //=> come dipendenza inserisco il valore movieList, che cambierà ogni volta che verrà selezionato un genere e di conseguenza andrà a influire su ciò che vedrò in pagina
 
   return (
     <>
       <h1>In proiezione al cinema!</h1>
-      {movieList.map((movie, index) => <article key={index}>
+
+      <select value={moviesList.genre} onChange={(e) => setMoviesList(e.target.value)}>
+        <option value=""></option>
+        <option>Azione</option>
+        <option>Fantascienza</option>
+        <option>Romantico</option>
+        <option>Thriller</option>
+      </select>
+
+
+      {filteredMovie.map((movie, index) => <article key={index}>
         <h3>{movie.title}</h3>
         <span>Genere: <em>{movie.genre}</em></span>
       </article>)}
