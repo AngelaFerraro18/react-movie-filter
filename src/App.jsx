8c492f67126e760bb7eac60f1c72f49fe2f1bc13
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-10 15:31:51 +0200

    feat: add input to search a movie by title using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ function App() {
   //salvo in una variabile il valore di partenza del filtro dei film, che sarà il mio array di partenza
   const [filteredMovie, setFilteredMovie] = useState(movies);
 
+  //salvo in una variabile il valore di partenza per il campo di input aggiunto per la ricerca per titolo
+  const [writeField, setWriteField] = useState('');
+
   //vado ad usare useEffect per filtrare la mia lista
   useEffect(() => {
 
@@ -37,6 +40,27 @@ function App() {
     }
   }, [moviesList]) //=> come dipendenza inserisco il valore movieList, che cambierà ogni volta che verrà selezionato un genere e di conseguenza andrà a influire su ciò che vedrò in pagina
 
+  useEffect(() => {
+    //salvo in una variabile result il valore del mio array di partenza
+    let result = movies;
+
+    //mi chiedo se il mio campo di input è diverso da una stringa vuota e uso il metodo trim() per eliminare eventuali spazi presenti nella stringa scritta, se il risultato è true allora procedo con la ricerca
+    if (writeField.trim() !== '') {
+
+      result = result.filter(movie => {
+
+        //salvo in due variabili il valore della chiave title e lo trasformo in minuscolo, stessa cosa procedo per il campo che verrà riempito dall'utente in modo da verificare due stringhe entrambe minuscole
+        const movieTitleLow = movie.title.toLowerCase();
+        const writeTitleLow = writeField.toLowerCase();
+
+        //essendo stringhe uso il metodo includes per verificare se le due stringhe siano uguali
+        return movieTitleLow.includes(writeTitleLow);
+      })
+    }
+    //di conseguenza andrò a mostrare il risultato, se avrò un match vedrò gli elementi che corrispondono alla ricerca, altrimenti vedrò l'intera lista
+    setFilteredMovie(result);
+  }, [writeField, moviesList])
+
   return (
     <>
       <h1>In proiezione al cinema!</h1>
@@ -49,6 +73,7 @@ function App() {
         <option>Thriller</option>
       </select>
 
+      <input type="text" value={writeField} onChange={e => setWriteField(e.target.value)} placeholder="Cerca per titolo..." />
 
       {filteredMovie.map((movie, index) => <article key={index}>
         <h3>{movie.title}</h3>
